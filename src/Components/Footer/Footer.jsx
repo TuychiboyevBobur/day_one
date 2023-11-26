8f997d44f4344397cea00286cbbb61d865c75745
TuychiboyevBobur <toychiboyevbobur16@gmail.com>
2023-11-26 16:59:36 +0500

    second commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../images/Logo.png";
+import Logo from "../images/logo.png";
 import "./footer.css";
 import {
   FaFacebook,
